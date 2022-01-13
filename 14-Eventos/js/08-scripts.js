0f6e00554212a77ecef1ad7cb91c4e18c0f3cd43
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-01-13 10:55:58 -0600

    Prevenir Event Bubbling con un mètodo

--- 14-Eventos/js/08-scripts.js
+++ 14-Eventos/js/08-scripts.js
@@ -1,25 +1,30 @@
-// Evitar la propagación con contenido creado...
-const parrafo1 = document.createElement('P');
-parrafo1.textContent = 'Concierto';
-parrafo1.classList.add('categoria');
-parrafo1.classList.add('concierto');
+// Evitar la propagación con contenido creado
 
-// Segundo parrafo
-const parrafo2 = document.createElement('P');
-parrafo2.textContent = 'Concierto de Rock';
-parrafo2.classList.add('titulo');
+// 1er parrafo
+const parafo01 = document.createElement('P');
+parafo01.textContent = 'Concierto';
+parafo01.classList.add('categoria');
+parafo01.classList.add('concierto');
 
-// 3er parrafo...
-const parrafo3 = document.createElement('p');
-parrafo3.textContent = '$800 por persona';
-parrafo3.classList.add('precio');
+// 2do parrafo
+const parafo02 = document.createElement('P');
+parafo02.textContent = 'Concierto de Rock';
+parafo02.classList.add('titulo');
 
-// crear el div...
+// 3er parrafo
+const parafo03 = document.createElement('p');
+parafo03.textContent = '$800 por persona';
+parafo03.classList.add('precio');
+parafo03.onclick = function(){
+    nuevaFuncion(1);
+};
+
+// crear el div
 const info = document.createElement('div');
 info.classList.add('info');
-info.appendChild(parrafo1)
-info.appendChild(parrafo2)
-info.appendChild(parrafo3);
+info.appendChild(parafo01)
+info.appendChild(parafo02)
+info.appendChild(parafo03);
 
 // Vamos a crear la imagen
 const imagen = document.createElement('img');
@@ -38,3 +43,9 @@ contenedorCard.appendChild(info);
 // Insertarlo en el HTML...
 const contenedor = document.querySelector('.hacer .contenedor-cards');
 contenedor.appendChild(contenedorCard); // al inicio info
+
+
+
+function nuevaFuncion(id){
+    console.log('desde nueva funcion', id);
+}
