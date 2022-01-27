572b123d4e5f7c46a63d825284cfb71c5418f2f4
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-01-26 22:15:47 -0600

    Administrador de presupuesto

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -1,7 +1,7 @@
 // Variables y selectores
 const formulario = document.querySelector('#agregar-gasto');
 const listadoGastos = document.querySelector('#gastos ul');
-let presupuesto;
+let presupuesto01;
 
 // Eventos
 eventListeners();
@@ -26,6 +26,11 @@ class Presupuesto {
     calcularRestante(){
         const gastado = this.gastos.reduce((total, gasto) => total + gasto.cantidad, 0);
         this.restante = this.presupuesto - gastado;
+    }
+
+    eliminarGasto(id){
+        this.gastos = this.gastos.filter(gasto => gasto.id !== id);
+        this.calcularRestante();
     }
 }
 
@@ -61,7 +66,7 @@ class UI {
         }, 3000 );
     }
 
-    agregarGastoListado(gastos){
+    mostrarGastos(gastos){
         // Elimina el html previo
         this.limpiarHTML(); 
 
@@ -81,7 +86,9 @@ class UI {
             const btnBorrar = document.createElement('button');
             btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
             btnBorrar.innerHTML = 'Borrar';
-
+            btnBorrar.onclick = () => {
+                eliminarGasto(id);
+            }
             nuevoGasto.appendChild(btnBorrar);
 
             // Agregar el HTML
@@ -98,6 +105,36 @@ class UI {
     actualizarRestante(restante){
         document.querySelector('#restante').textContent = restante;
     }
+
+    comprobarPresupuesto(presupuestoObj){
+        const {presupuesto, restante} = presupuestoObj;
+
+        const restanteDiv = document.querySelector('.restante');
+
+
+        // Comprobar el 25% restante del presupuesto
+        if((presupuesto / 4) > restante){
+            console.log((presupuesto / 4), restante);
+            restanteDiv.classList.remove('alert-success', 'alert-warning');
+            restanteDiv.classList.add('alert-danger');
+        } 
+        // Comprobar el 50% restante del presupuesto
+        else if ((presupuesto / 2) > restante) {
+            restanteDiv.classList.remove('alert-success');
+            restanteDiv.classList.add('alert-warning');
+        } 
+        // Comprobamos si un gasto es reintegrado
+        else {
+            restanteDiv.classList.remove('alert-danger', 'alert-warning');
+            restanteDiv.classList.add('alert-success');
+        }
+
+        // Si el total es menor o igual a 0
+        if(restante <= 0){
+            ui.imprimirAlerta('El presupuesto se ha agotado', 'error');
+            formulario.querySelector('button[type="submit"]').disabled = true;
+        }
+    }
 }
 
 // Instanciar
@@ -111,9 +148,9 @@ function pedirPresupuerto(){
     }
 
     // Presupuesto valido
-    presupuesto = new Presupuesto(presupuestoUsuario);
+    presupuesto01 = new Presupuesto(presupuestoUsuario);
 
-    ui.inserPresupuesto(presupuesto);
+    ui.inserPresupuesto(presupuesto01);
 }
 
 // Añadir gastos
@@ -137,17 +174,32 @@ function agregarGastos(event){
     const gasto = {nombre, cantidad, id: Date.now()};
 
     // Añadiendo un nuevo gasto
-    presupuesto.nuevoGasto(gasto);
+    presupuesto01.nuevoGasto(gasto);
 
     // Mensaje de gasto agregado con exito
     ui.imprimirAlerta('El gasto fue agregado correctamente', 'success');
 
     // Añadir el gasto al listado
-    const { gastos, restante } = presupuesto;
-    ui.agregarGastoListado(gastos);
+    const { gastos, restante } = presupuesto01;
+    ui.mostrarGastos(gastos);
 
     ui.actualizarRestante(restante);
 
+    ui.comprobarPresupuesto(presupuesto01);
+
     // Reiniciamos el formulario
     formulario.reset();
+}
+
+function eliminarGasto(id){
+    // Elimina los gastos del objeto
+    presupuesto01.eliminarGasto(id);
+
+    // Elimina los gastos del HTML
+    const {gastos, restante} = presupuesto01;
+    ui.mostrarGastos(gastos);
+
+    ui.actualizarRestante(restante);
+
+    ui.comprobarPresupuesto(presupuesto01);
 }
